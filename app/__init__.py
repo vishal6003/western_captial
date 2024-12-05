e92937488e40ff048980d28cad63d9edbe9f3b19
Intune Admin <admin@nafa.co.in>
2024-12-05 18:04:24 +0530

    Updated __init__.py with [brief description of changes]

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ def create_app():
     CORS(app, resources={
         r"/*": {
             "origins": [
-                "http://localhost:4200", 
+                "https://wester-captial-test.web.app/", 
             ],
             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             "allow_headers": ["Content-Type"]
